84fd410e28cb2fb1b41b51e5cb2781cb55360bec
Matheus-Aguilar <matheusdeoliveira.ti@gmail.com>
2020-11-08 23:29:10 -0300

    Botões e Localização Adicionados

--- app/src/main/java/android/teste/matheusaguilar/maps/MainActivity.java
+++ app/src/main/java/android/teste/matheusaguilar/maps/MainActivity.java
@@ -2,7 +2,9 @@ package android.teste.matheusaguilar.maps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void abreMapa(View view) {
+        Intent it = new Intent(getBaseContext(), MapsActivity.class);
+        it.putExtra("local", view.getTag().toString());
+        startActivity(it);
+    }
+
+    public void fechar(View view){
+        finish();
+    }
+
 }
--- app/src/main/java/android/teste/matheusaguilar/maps/MapsActivity.java
+++ app/src/main/java/android/teste/matheusaguilar/maps/MapsActivity.java
@@ -1,13 +1,29 @@
 package android.teste.matheusaguilar.maps;
 
+import androidx.appcompat.widget.ActivityChooserView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewDebug;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -15,9 +31,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
 
+    private String local;
+
+    private final LatLng NATAL = new LatLng(-16.374775, -40.537228);
+    private final LatLng VICOSA = new LatLng(-20.753199, -42.878001);
+    private final LatLng DPI = new LatLng(-20.764993, -42.868467);
+    private LatLng ATUAL = new LatLng(0.0, 0.0);
+
+    public final int LOCATION_PERMISSION = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        local = getIntent().getStringExtra("local");
+
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -37,10 +65,87 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        atualizaMapa();
+//        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    private void atualizaMapa(){
+        switch(local){
+            case "cidadeNatal":
+                mMap.addMarker(new MarkerOptions().position(NATAL).title("Rubim-MG"));
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(NATAL, 16));
+                break;
+            case "casaVicosa":
+                mMap.addMarker(new MarkerOptions().position(VICOSA).title("Viçosa-MG"));
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(VICOSA, 16));
+                break;
+            case "departamento":
+                mMap.addMarker(new MarkerOptions().position(DPI).title("DPI"));
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(DPI, 16));
+                break;
+            case "atual":
+                mMap.addMarker(new MarkerOptions().position(ATUAL).title("Localização Atual")
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(ATUAL, 16));
+
+                float results[] = new float[3];
+                Location.distanceBetween(VICOSA.latitude, VICOSA.longitude, ATUAL.latitude, ATUAL.longitude, results);
+
+                Toast.makeText(this, "Você está a " + Float.toString(results[0]) + " metros de sua casa em Viçosa",
+                        Toast.LENGTH_LONG).show();
+
+                break;
+        }
+    }
+
+    public void trocaFoco(View view){
+        local = view.getTag().toString();
+        atualizaMapa();
+    }
+
+    public void localizacaoAtual(View view){
+        requestLocationPermission();
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+
+            LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            String provider = LocationManager.NETWORK_PROVIDER;
+
+            Location loc = lm.getLastKnownLocation(provider);
+
+            ATUAL = new LatLng(loc.getLatitude(), loc.getLongitude());
+            local = "atual";
+
+            atualizaMapa();
+        }
+    }
+
+    public void requestLocationPermission(){
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+                Toast.makeText(this,"Permita o uso da localização para acessar seu local", Toast.LENGTH_LONG).show();
+            }
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int code, String permission[], int[] grantResults){
+        switch(code){
+            case LOCATION_PERMISSION:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this, "Permissão Concedida", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Toast.makeText(this, "Permissão Negada: não é possível buscar a localização", Toast.LENGTH_LONG);
+                }
+            break;
+        }
     }
 }
